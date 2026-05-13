1037ed10de4f4ff50822ac7cec2c88a49c6c8061
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-05-13 21:28:59 +0530

    Added controller logic and DTO file

---
+++ backend/src/main/java/com/smartmail_assistant/service/EmailGeneratorService.java
@@ -0,0 +1,12 @@
+package com.smartmail_assistant.service;
+
+import com.smartmail_assistant.dto.EmailRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+    public String generateEmailReply(EmailRequest emailRequest){
+        return "TODO";
+    }
+
+}
