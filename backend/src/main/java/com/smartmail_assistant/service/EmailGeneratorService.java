82b66f4131a635504947b3ffb7addd9ea74f385c
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-05-14 15:10:00 +0530

    Implemented buildPrompt method

--- backend/src/main/java/com/smartmail_assistant/service/EmailGeneratorService.java
+++ backend/src/main/java/com/smartmail_assistant/service/EmailGeneratorService.java
@@ -5,8 +5,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailGeneratorService {
-    public String generateEmailReply(EmailRequest emailRequest){
+    public String generateEmailReply(EmailRequest emailRequest) {
+        //Build the prompt
+        String prompt = buildPrompt(emailRequest);
+        //Craft the request
+        //do request and get response
+        //return response
         return "TODO";
     }
 
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Write a professional email reply. No subject line");
+        if (emailRequest.getTone() != null && !emailRequest.getTone().isBlank()) {
+            prompt.append("Tone: ").append(emailRequest.getTone()).append(".");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
 }
